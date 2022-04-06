6ff0e04f64b4814f126a6920aa534a8a4b6658b8
edmondthui <edmond.t.hui@gmail.com>
2022-04-06 17:46:44 -0400

    feat: add routes to app

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react"; // for top level parents if you don't want to render a html element no need for wrapping div
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <CrownLogo className="logo" />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="/shop">
+                        SHOP
+                    </Link>
+                    <Link className="nav-link" to="/sign-in">
+                        SIGN IN
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
+};
+
+export default Navigation;
