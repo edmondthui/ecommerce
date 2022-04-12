f8e923c1cb08e14e5524e61b6d4b5d8cb3fc11f5
edmondthui <edmond.t.hui@gmail.com>
2022-04-12 19:21:09 -0400

    feat: added in add to cart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,24 @@
+import {useContext} from 'react';
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
+import {CartContext} from '../../contexts/cart.context'
+
+
 
 
 const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext)
     return (
         <div className="cart-dropdown-container">
             <div className="cart-items">
+                {cartItems.map((item) => (
+                    <CartItem cartItem={item} />
+                ))}
             </div>
             <Button>Go to checkout</Button>
         </div>
-    )
+    );
 }
 
 export default CartDropdown;
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,15 +1,24 @@
+import { useContext} from 'react'
 import "./product-card.styles.scss";
+import {CartContext} from '../../contexts/cart.context'
 import Button from "../button/button.component"
+
 const ProductCard = ({product}) => {
+    const {addItemToCart} = useContext(CartContext);
     const {name, price, imageUrl} = product;
-    return (<div className="product-card-container">
-        <img src = {imageUrl} alt ={`${name}`}/>
-        <div className="footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
+
+    const addToCart = () => addItemToCart(product);
+    
+    return (
+        <div className="product-card-container" >
+            <img src={imageUrl} alt={`${name}`} />
+            <div className="footer">
+                <span className="name">{name}</span>
+                <span className="price">{price}</span>
+            </div>
+            <Button buttonType="inverted" onClick={addToCart}>Add to cart</Button>
         </div>
-        <Button buttonType="inverted">Add to cart</Button>
-    </div>)
+    );
 };
 
 export default ProductCard;
